f784a69985f92317f57f8d66bbe4d98511e76449
Philippe Van Eerdenbrugghe <philippe.vaneerdenbrugghe@tapptic.com>
2012-07-12 11:43:45 +0200

    use proper way to get config values

--- lib/mass_assignable/railtie.rb
+++ lib/mass_assignable/railtie.rb
@@ -4,13 +4,14 @@ require 'active_support'
 
 module MassAssignable
   class Railtie < Rails::Railtie
-    initializer "mass_assignable.hook_into_active_record", :after => "finisher_hook " do
+    initializer "mass_assignable.hook_into_active_record", :after => "finisher_hook " do |app|
       ActiveSupport.on_load(:active_record) do
         # ugly way to ensure a connection is available
         unless ActiveRecord::Base.connected?
-          ActiveRecord::Base.configurations= configuration.database_configuration
+          ActiveRecord::Base.configurations= app.config.database_configuration
           ActiveRecord::Base.establish_connection
         end
+
         require 'mass_assignable/active_record_extension'
         ActiveRecord::Base.send(:include, MassAssignable::ActiveRecordExtension)
       end
