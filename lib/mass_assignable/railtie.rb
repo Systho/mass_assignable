20c2c31f3d73acb5018a60dbf115955e3c095682
Philippe Van Eerdenbrugghe <philippe.vaneerdenbrugghe@tapptic.com>
2012-07-12 11:31:08 +0200

    establish connection if needed

--- lib/mass_assignable/railtie.rb
+++ lib/mass_assignable/railtie.rb
@@ -6,6 +6,7 @@ module MassAssignable
   class Railtie < Rails::Railtie
     initializer "mass_assignable.hook_into_active_record", :after => "finisher_hook " do
       ActiveSupport.on_load(:active_record) do
+        ActiveRecord::Base.establish_connection unless ActiveRecord::Base.connected?
         require 'mass_assignable/active_record_extension'
         ActiveRecord::Base.send(:include, MassAssignable::ActiveRecordExtension)
       end
